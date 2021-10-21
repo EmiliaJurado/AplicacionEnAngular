3ab39d9bc355f334517f04e0b12ce323fd77a8ad
EmiliaJurado <emily_9814@hotmail.com>
2021-10-21 09:05:29 -0500

    version1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+ 
 })
 export class AppComponent {
-  title = 'primero';
+  public title:string = 'Contador con angular';
+  public numero:number = 0;
+
+  //Base 5
+  public sumarRestar(valor:number){
+    this.numero = this.numero + valor;
+  }
 }
